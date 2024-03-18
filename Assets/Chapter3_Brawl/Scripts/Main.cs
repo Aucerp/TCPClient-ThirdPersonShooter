80bdf538d7e098648155a453017db6754ac15e2e
Aucer <aucerp@gmail.com>
2024-03-18 15:17:37 +0800

    Now use Main.cs to instantiate HumanPlayer.

--- Assets/Chapter3_Brawl/Scripts/Main.cs
+++ Assets/Chapter3_Brawl/Scripts/Main.cs
@@ -1,18 +1,69 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Main : MonoBehaviour
 {
+    //Prefab
+    public GameObject humanPrefab;
+    //人物列表
+    private BaseHuman myHuman;
+    public Dictionary<string, BaseHuman> otherHumans = new Dictionary<string, BaseHuman>();
     // Start is called before the first frame update
     void Start()
     {
-        
+        NetManager.AddListener("Enter", OnEnter);
+        NetManager.AddListener("Move", OnMove);
+        NetManager.AddListener("Leave", OnLeave);
+        NetManager.Connect("127.0.0.1", 8888);
+
+        //添加一個角色
+        GameObject obj = Instantiate(humanPrefab) as GameObject;
+        float x = UnityEngine.Random.Range(-5, 5);
+        float z = UnityEngine.Random.Range(-5, 5);
+        obj.transform.position = new Vector3(x, 0, z);
+        myHuman = obj.AddComponent<CtrlHuman>();
+        myHuman.desc = NetManager.GetDesc();
+
+        //發送協議
+        Vector3 pos = myHuman.transform.position;
+        Vector3 eul = myHuman.transform.eulerAngles;
+        string sendStr = "Enter|" +
+            NetManager.GetDesc() +
+            "," + pos.x + "," +
+            pos.y + "," +
+            pos.z + "," +
+            eul.y;
+        NetManager.Send(sendStr);
+    }
+    private void Update()
+    {
+        NetManager.Update();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnter(string msgArgs)
     {
-        
+        Debug.Log("OnEnter " + msgArgs);
+        //解析參數
+        string[] split = msgArgs.Split(',');
+        string desc = split[0];
+        float x = float.Parse(split[1]);
+        float y = float.Parse(split[2]);
+        float z = float.Parse(split[3]);
+        float eulY = float.Parse(split[4]);
+        //是自己
+        if(desc == NetManager.GetDesc())
+            return;
+        //添加角色
+        GameObject obj = Instantiate(humanPrefab) as GameObject;
+        obj.transform.position = new Vector3(x, y, z);
+        obj.transform.eulerAngles = new Vector3(0, eulY, 0);
+        BaseHuman h = obj.AddComponent<SyncHuman>();
+        h.desc = desc;
+        otherHumans.Add(desc, h);
     }
+    private void OnMove(string msg) { Debug.Log("OnMove " + msg); }
+    private void OnLeave(string msg) { Debug.Log("OnLeave " + msg); }
 }
